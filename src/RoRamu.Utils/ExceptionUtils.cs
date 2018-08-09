0955318a7a319d32ca603921fa884bb1a5b35b6a
Unknown <rohitramu@gmail.com>
2018-08-08 21:51:15 -0700

    Add method to convert exception to JSON string

--- src/RoRamu.Utils/ExceptionUtils.cs
+++ src/RoRamu.Utils/ExceptionUtils.cs
@@ -4,16 +4,29 @@
 
     public static class ExceptionUtils
     {
-        public static string ToMessageString(this Exception exception, bool includeStackTraceAndExceptionType = false)
+        public static string ToJsonString(this Exception exception, bool includeStackTraceAndExceptionType = false)
         {
             if (exception == null)
             {
                 throw new ArgumentNullException(nameof(exception));
             }
 
-            string result = includeStackTraceAndExceptionType
-                ? exception.ToString()
-                : exception.Message;
+            object obj;
+            if (includeStackTraceAndExceptionType)
+            {
+                obj = new
+                {
+                    Type = exception.GetType().FullName,
+                    Message = exception.Message,
+                    StackTrace = exception.StackTrace,
+                };
+            }
+            else
+            {
+                obj = exception.Message;
+            }
+
+            string result = JsonUtils.SerializeToJson(obj);
 
             return result;
         }
