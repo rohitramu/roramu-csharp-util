e589871b1acecd9cbd45fd2aa9d00e502a5a5316
Unknown <rohitramu@gmail.com>
2018-08-08 19:20:44 -0700

    Add ExceptionUtils

---
+++ src/RoRamu.Utils/ExceptionUtils.cs
@@ -0,0 +1,21 @@
+﻿namespace RoRamu.Utils
+{
+    using System;
+
+    public static class ExceptionUtils
+    {
+        public static string ToMessageString(this Exception exception, bool includeStackTraceAndExceptionType = false)
+        {
+            if (exception == null)
+            {
+                throw new ArgumentNullException(nameof(exception));
+            }
+
+            string result = includeStackTraceAndExceptionType
+                ? exception.ToString()
+                : exception.Message;
+
+            return result;
+        }
+    }
+}
