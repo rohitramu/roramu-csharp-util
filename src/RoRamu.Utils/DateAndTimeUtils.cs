75a9af724a9129d2d9fb24a2e85ea7863282601b
Unknown <rohitramu@gmail.com>
2018-08-11 18:17:11 -0700

    Add date and time utils

---
+++ src/RoRamu.Utils/DateAndTimeUtils.cs
@@ -0,0 +1,60 @@
+﻿namespace RoRamu.Utils
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class DateAndTimeUtils
+    {
+        public static string ToFormattedString(this TimeSpan span)
+        {
+            // Handle the case where the timespan is zero
+            if (span == TimeSpan.Zero)
+            {
+                return "0 seconds";
+            }
+
+            IList<string> stringParts = new List<string>();
+
+            // Days
+            if (span.Days != 0)
+            {
+                string daysLabel = span.Days == 1 ? "day" : "days";
+                stringParts.Add($"{span.Days} {daysLabel}");
+            }
+
+            // Hours
+            if (span.Hours != 0)
+            {
+                string hoursLabel = span.Hours == 1 ? "hour" : "hours";
+                stringParts.Add($"{span.Hours} {hoursLabel}");
+            }
+
+            // Minutes
+            if (span.Minutes != 0)
+            {
+                string minutesLabel = span.Minutes == 1 ? "minute" : "minutes";
+                stringParts.Add($"{span.Minutes} {minutesLabel}");
+            }
+
+            // Seconds and milliseconds
+            if (span.Seconds != 0 || span.Milliseconds != 0)
+            {
+                string millisecondsPart = span.Milliseconds == 0 ? string.Empty : $".{span.Milliseconds}";
+                string secondsLabel = span.Seconds == 1 && span.Milliseconds == 0 ? "second" : "seconds";
+                stringParts.Add($"{span.Seconds}{millisecondsPart} {secondsLabel}");
+            }
+
+            // Include the word "and" before the last part
+            if (stringParts.Count > 1)
+            {
+                stringParts.Insert(stringParts.Count - 1, "and");
+            }
+
+            // Handle negative timespan
+            string result = span < TimeSpan.Zero ? "-" : string.Empty;
+            result += string.Join(' ', stringParts);
+
+            return result;
+        }
+    }
+}
