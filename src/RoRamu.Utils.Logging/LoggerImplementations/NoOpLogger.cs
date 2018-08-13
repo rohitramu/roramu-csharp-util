83b2ee69d9609b81c28e9e7ee3798d56ecc233ed
Unknown <rohitramu@gmail.com>
2018-08-13 00:11:26 -0700

    Add No-Op Logger implementation

---
+++ src/RoRamu.Utils.Logging/LoggerImplementations/NoOpLogger.cs
@@ -0,0 +1,14 @@
+﻿namespace RoRamu.Utils.Logging
+{
+    public class NoOpLogger : Logger
+    {
+        protected override void HandleLog<T>(
+            LogLevel logLevel,
+            string message,
+            T extraInfo,
+            string callerName,
+            string sourceFilePath,
+            int sourceLineNumber)
+        { }
+    }
+}
