8f8c6cec4f431045932e7ca960b2f058a36be3aa
Rohit Ramu <rohitramu@gmail.com>
2021-03-15 10:14:03 -0700

    Add helper method for throwing on failure

---
+++ src/RoRamu.Utils.Messaging/RequestResultExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace RoRamu.Utils.Messaging
+{
+    using System;
+
+    /// <summary>
+    /// Extension methods for <see cref="RequestResult" /> objects.
+    /// </summary>
+    public static class RequestResultExtensions
+    {
+        /// <summary>
+        /// Throws an exception if the request failed.
+        /// </summary>
+        /// <param name="result"></param>
+        public static void ThrowOnError(this RequestResult result)
+        {
+            if (result == null)
+            {
+                throw new ArgumentNullException(nameof(result));
+            }
+
+            if (!result.IsSuccessful)
+            {
+                throw result.Exception ?? new ErrorResponseException("Unexpected failure - no exception found in error response.", null);
+            }
+        }
+    }
+}
